257d641999ae8c1f558a047d27bf9ed9479b10b6
chinzo <chinzo@localhost>
2017-04-21 12:20:12 -0500

    change on HomeController

--- src/main/java/com/pm/onlinetest/controller/HomeController.java
+++ src/main/java/com/pm/onlinetest/controller/HomeController.java
@@ -23,10 +23,10 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = {"/", "welcome"}, method = RequestMethod.GET)
+	@RequestMapping(value = {"/", "home"}, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		return "";
+		return "home";
 	}
 	
 }
