88d2e553c145d5b8f55b7495a4e5e86d897e0cb9
chinzo <chinzo@localhost>
2017-04-24 11:08:09 -0500

    Admin section

---
+++ src/main/java/com/pm/onlinetest/controller/AdminController.java
@@ -0,0 +1,81 @@
+package com.pm.onlinetest.controller;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.pm.onlinetest.domain.User;
+import com.pm.onlinetest.domain.Authority;
+import com.pm.onlinetest.service.AuthorityService;
+import com.pm.onlinetest.service.UserService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+	@Autowired
+	UserService userService;
+	@Autowired
+	AuthorityService authorityService;
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "admin-home";
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public String roleCheck(Locale locale, Model model) {
+		List<User> users = userService.findAll();
+		model.addAttribute("users", users);
+		return "users";
+	}
+
+	@RequestMapping(value="/register", method = RequestMethod.GET)
+	public String register(@ModelAttribute("loginUser") User user) {
+ 		return "register";
+	}
+	
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String add(@Valid @ModelAttribute("loginUser") User user, BindingResult result, 
+			RedirectAttributes redirectAttr) {
+		if(result.hasErrors()) {
+			return "register";
+		}
+		
+		userService.save(user);
+		redirectAttr.addFlashAttribute("success", "Successfully added new user!");
+	   	return "redirect:/admin/register";
+	}
+	
+	@RequestMapping(value = { "/deleteUser" }, method = RequestMethod.POST)
+	public void DeletePost(HttpServletRequest request) {
+		String id = request.getParameter("userid").toString();
+		User user = userService.findByUserId(Integer.parseInt(id));
+		authorityService.delete(authorityService.findByUser(user));
+		userService.delete(user);
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/tiles/admin-menu.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/views/include.jsp"%>
+<div class="hor-menu ">
+	<ul class="nav navbar-nav">
+		<li><a href="<c:url value="/admin/home" />">Home</a></li>
+		<li><a href="<c:url value="/admin/register" />">User Register</a></li>
+		<li><a href="<c:url value="/admin/users" />">User List</a></li>
+		<li><a href="<c:url value="/admin/coaches" />">Coach List</a></li>
+		<li><a href="<c:url value="/admin/coaches" />">Student List</a></li>
+	</ul>
+</div>
