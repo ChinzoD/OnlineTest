b2a27237287a7d382fef47ec70be6c47130cf0d7
chinzo <chinzo@localhost>
2017-04-27 10:09:39 -0500

    test

--- src/main/java/com/pm/onlinetest/controller/AdminController.java
+++ src/main/java/com/pm/onlinetest/controller/AdminController.java
@@ -62,68 +62,69 @@ public class AdminController {
 		return "users";
 	}
 
-	@RequestMapping(value="/register", method = RequestMethod.GET)
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String register(@ModelAttribute("loginUser") User user) {
- 		return "register";
+		return "register";
 	}
-	
-	@RequestMapping(value="/register", method = RequestMethod.POST)
-	public String add(@Valid @ModelAttribute("loginUser") User user, BindingResult result, 
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String add(@Valid @ModelAttribute("loginUser") User user, BindingResult result,
 			RedirectAttributes redirectAttr) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return "register";
 		}
-		
+
 		userService.save(user);
 		redirectAttr.addFlashAttribute("success", "Successfully added new user!");
-	   	return "redirect:/admin/register";
+		return "redirect:/admin/register";
 	}
-	
-	@RequestMapping(value="/registerStudent", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/registerStudent", method = RequestMethod.GET)
 	public String getStudent(@ModelAttribute("loginUser") Student student) {
- 		return "registerStudent";
+		return "registerStudent";
 	}
-	
-	@RequestMapping(value="/registerStudent", method = RequestMethod.POST)
-	public String registerStudent(@ModelAttribute("loginUser") Student student, BindingResult result, 
+
+	@RequestMapping(value = "/registerStudent", method = RequestMethod.POST)
+	public String registerStudent(@ModelAttribute("loginUser") Student student, BindingResult result,
 			RedirectAttributes redirectAttr) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return "registerStudent";
 		}
-		
+
 		studentService.save(student);
 		redirectAttr.addFlashAttribute("success", "Successfully added new user!");
-	   	return "redirect:/admin/registerStudent";
+		return "redirect:/admin/registerStudent";
 	}
-	
+
 	@RequestMapping(value = { "/deleteUser" }, method = RequestMethod.POST)
 	public void DeleteUser(HttpServletRequest request) {
 		String id = request.getParameter("userid").toString();
 		User user = userService.findByUserId(Integer.parseInt(id));
 		userService.delete(user);
 	}
-	
+
 	@RequestMapping(value = "/students", method = RequestMethod.GET)
 	public String getStudents(Locale locale, Model model) {
 		List<Student> students = studentService.findAll();
 		model.addAttribute("students", students);
 		return "students";
 	}
-	
+
 	@RequestMapping(value = "/assign", method = RequestMethod.GET)
 	public String assignCoach(Locale locale, Model model) {
 		List<Student> students = studentService.findAll();
 		List<User> coaches = userService.findByAuthority("ROLE_COACH");
-		
+
 		model.addAttribute("students", students);
 		model.addAttribute("coaches", coaches);
-		
+
 		return "assignCoach";
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/assign", method = RequestMethod.POST)
-	public String getAssignCoach(Locale locale, Model model, HttpServletRequest request, RedirectAttributes redirectAttr) {
+	public String getAssignCoach(Locale locale, Model model, HttpServletRequest request,
+			RedirectAttributes redirectAttr) {
 		String coachId = request.getParameter("coachId").toString();
 		String studentId = request.getParameter("studentId").toString();
 		User coach = userService.findByUserId(Integer.parseInt(coachId));
@@ -132,19 +133,19 @@ public class AdminController {
 		studentRecord.setCoach(coach);
 		studentRecord.setStudent(student);
 		studentRecordService.save(studentRecord);
-		
-		//redirectAttr.addFlashAttribute("success", "Successfully assigned!");
-		//return "assigned";
-	   	return "ok";
+
+		// redirectAttr.addFlashAttribute("success", "Successfully assigned!");
+		// return "assigned";
+		return "ok";
 	}
-	
+
 	@RequestMapping(value = "/assignedList", method = RequestMethod.GET)
 	public String getAssignedList(Locale locale, Model model) {
 		List<Student_Record> studentRecords = studentRecordService.findAll();
 		model.addAttribute("studentRecords", studentRecords);
 		return "assignedList";
 	}
-	
+
 	@RequestMapping(value = { "/deleteAssign" }, method = RequestMethod.POST)
 	public void DeleteAssign(HttpServletRequest request) {
 		String id = request.getParameter("userid").toString();
