2ecf9e2f311a808b6a01f5c062099d27f98f2480
Birhanu Gebresenbet <birhanu2050@gmail.com,birhanu.gebresenbet@gmail.com>
2017-05-15 00:54:49 -0500

    Comment added to question service, controller, repository

--- src/main/java/com/pm/onlinetest/service/QuestionService.java
+++ src/main/java/com/pm/onlinetest/service/QuestionService.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 import com.pm.onlinetest.domain.Question;
 import com.pm.onlinetest.domain.Subcategory;
-
+/**
+* The Question service interfce have used to add,update and delelte question and its corresponding choices and answers
+* It also used to for shared by  other modules .
+*
+* @author  Birhanu Gebresenbet
+* @version 1.0
+* @since   2017-03-31 
+*/
 public interface QuestionService {
 	
 	public void save(Question question);
